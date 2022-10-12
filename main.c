cee977a7f4ee2931e5511224c0c18899ca3170a2
Nguyễn Duy Hòa <77421904+orangethefish@users.noreply.github.com>
2022-10-12 15:29:52 +0700

    Update main.c

--- main.c
+++ main.c
@@ -52,7 +52,7 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-void display7SEG(int num){
+void display7SEG(int num){ //change 7seg led according to num
 	if(num == 0){
 			HAL_GPIO_WritePin(A_GPIO_Port, A_Pin, RESET);
 			HAL_GPIO_WritePin(B_GPIO_Port, B_Pin, RESET);
